81019cb9a1854b8872ecb1e71a9365bae0f3fed2
Daniel <daniel.brueggemann.mail@gmail.com>
2016-01-26 14:18:54 +0100

    Made board size configurable by console.

--- Final Version/src/cgtsolver/GameMain.java
+++ Final Version/src/cgtsolver/GameMain.java
@@ -1,16 +1,45 @@
 package cgtsolver;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Main class, entry point for starting the solver. The results are printed out
  * inside the solver search.
+ *
  * @author daniel
  */
 public class GameMain
 {
   public static void main(String args[])
   {
+    int width = -1;
+    int height = -1;
+    while (width == -1 || height == -1)
+    {
+      try
+      {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter board width: ");
+        width = in.nextInt();
+        System.out.print("Enter board height: ");
+        height = in.nextInt();
+        
+        if(width <= 0 || height <= 0)
+        {
+          width = -1;
+          height = -1;
+        }
+      }
+      catch (InputMismatchException ex)
+      {
+        System.out.println("Invalid user input: Width/Height has to be an integer > 0.");
+      }
+    }
+
+    System.out.println("Running solver ...");
     AbSolver abSolver = new AbSolver();
-    GameState gameState = new GameState(5,5);
+    GameState gameState = new GameState(width, height);
     abSolver.search(gameState);
   }
 }
